bb149cdff0e60c74bbcd0765d182d087fbb4b952
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2022-04-18 18:53:56 -0400

    move w2v2 module classes to separate file

---
+++ robust_speech/models/modules/hf_wav2vec2.py
@@ -0,0 +1,399 @@
+import logging
+import os
+import sys
+
+import speechbrain as sb
+import torch
+import torch.nn as nn
+from speechbrain.lobes.models.huggingface_wav2vec import HuggingFaceWav2Vec2
+from transformers import (
+    HubertConfig,
+    HubertModel,
+    Wav2Vec2Config,
+    Wav2Vec2FeatureExtractor,
+    Wav2Vec2Model,
+)
+
+import transformers
+from speechbrain.lobes.models.huggingface_wav2vec import HuggingFaceWav2Vec2Pretrain
+from transformers import Wav2Vec2ForPreTraining
+from transformers.file_utils import (
+    add_start_docstrings_to_model_forward,
+    replace_return_docstrings,
+)
+from transformers.models.wav2vec2.configuration_wav2vec2 import Wav2Vec2Config as Wav2Vec2PretrainConfig
+from transformers.models.wav2vec2.modeling_wav2vec2 import (
+    _CONFIG_FOR_DOC,
+    WAV_2_VEC_2_INPUTS_DOCSTRING,
+    Wav2Vec2ForPreTrainingOutput,
+    _compute_mask_indices,
+)
+from transformers.models.wav2vec2.modeling_wav2vec2 import Wav2Vec2FeatureExtractor as Wav2Vec2PretrainFeatureExtractor
+import robust_speech as rs
+
+
+class AdvWav2Vec2FeatureEncoder(Wav2Vec2PretrainFeatureExtractor):
+    """
+    Slight modification of the HF feature extractor.
+    The original class assumes that input is a leaf tensor,
+    which when running attacks isn't always the case.
+    """
+
+    def forward(self, input_values):
+        hidden_states = input_values[:, None]
+        # make sure hidden_states require grad for gradient_checkpointing
+        if (
+            self._requires_grad and self.training and hidden_states.is_leaf
+        ):  # not always true when attacking
+            hidden_states.requires_grad = True
+
+        for conv_layer in self.conv_layers:
+            if self._requires_grad and self.gradient_checkpointing and self.training:
+
+                def create_custom_forward(module):
+                    def custom_forward(*inputs):
+                        return module(*inputs)
+
+                    return custom_forward
+
+                hidden_states = torch.utils.checkpoint.checkpoint(
+                    create_custom_forward(conv_layer),
+                    hidden_states,
+                )
+            else:
+                hidden_states = conv_layer(hidden_states)
+
+        return hidden_states
+
+
+class AdvWav2Vec2ForPreTraining(Wav2Vec2ForPreTraining):
+    """
+    This class modifies the transformers Wav2Vec2ForPreTraining module in order to
+        -replace the Feature Extractor with AdvWav2Vec2FeatureEncoder
+        -handle contrastive attacks in forward
+    """
+
+    def __init__(self, config: Wav2Vec2PretrainConfig):
+        super().__init__(config)
+        self.wav2vec2.feature_extractor = AdvWav2Vec2FeatureEncoder(config)
+
+    @add_start_docstrings_to_model_forward(WAV_2_VEC_2_INPUTS_DOCSTRING)
+    @replace_return_docstrings(
+        output_type=Wav2Vec2ForPreTrainingOutput, config_class=_CONFIG_FOR_DOC
+    )
+    def forward(
+        self,
+        input_values,
+        attention_mask=None,
+        mask_time_indices=None,
+        sampled_negative_indices=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        quantized_representation=None,
+    ):
+        """
+        New argument quantized_representation contains an optional
+        precomputed value for (quantized_features, codevector_perplexity).
+        If available, this value is not recomputed in the foward pass.
+
+        Returns:
+        --------
+        """
+
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
+
+        if mask_time_indices is not None:
+            mask_time_indices = mask_time_indices.to(torch.bool)
+        outputs = self.wav2vec2(
+            input_values,
+            attention_mask=attention_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            mask_time_indices=mask_time_indices,
+            return_dict=return_dict,
+        )
+
+        # 1. project all transformed features (including masked) to final vq
+        # dim
+        transformer_features = self.project_hid(outputs[0])
+
+        # 2. quantize all (unmasked) extracted features and project to final vq
+        # dim
+        extract_features = self.dropout_features(outputs[1])
+
+        if attention_mask is not None:
+            # compute reduced attention_mask correponding to feature vectors
+            attention_mask = self._get_feature_vector_attention_mask(
+                extract_features.shape[1], attention_mask, add_adapter=False
+            )
+        if quantized_representation is not None:
+            quantized_features, codevector_perplexity = quantized_representation
+        else:
+            quantized_features, codevector_perplexity = self.quantizer(
+                extract_features, mask_time_indices=mask_time_indices
+            )
+            quantized_features = self.project_q(quantized_features)
+
+        loss = contrastive_loss = diversity_loss = None
+        if sampled_negative_indices is not None:
+            batch_size, sequence_length, hidden_size = quantized_features.shape
+
+            # for training, we sample negatives
+            # 3. sample K negatives (distractors) quantized states for contrastive loss
+            # if attention_mask is passed,
+            # make sure that padded feature vectors cannot be sampled
+            # sample negative quantized vectors BTC => (BxT)C
+            negative_quantized_features = quantized_features.view(-1, hidden_size)[
+                sampled_negative_indices.long().view(-1)
+            ]
+            negative_quantized_features = negative_quantized_features.view(
+                batch_size, sequence_length, -1, hidden_size
+            ).permute(2, 0, 1, 3)
+
+            # 4. compute logits, corresponding to `logs = sim(c_t, [q_t, \sim{q}_t]) / \kappa`
+            # of equation (3) in https://arxiv.org/pdf/2006.11477.pdf
+            logits = self.compute_contrastive_logits(
+                quantized_features[None, :],
+                negative_quantized_features,
+                transformer_features,
+                self.config.contrastive_logits_temperature,
+            )
+
+            # 5. if a negative vector is identical to the positive
+            # (i.e. when codebook utilization is low),
+            # its cosine similarity will be masked
+            neg_is_pos = (quantized_features == negative_quantized_features).all(-1)
+
+            if neg_is_pos.any():
+                logits[1:][neg_is_pos] = float("-inf")
+
+            # 6. compute contrastive loss \mathbf{L}_m = cross_entropy(logs) =
+            # -log(exp(sim(c_t, q_t)/\kappa) / \sum_{\sim{q}} exp(sim(c_t, \sim{q})/\kappa))
+            logits = logits.transpose(0, 2).reshape(-1, logits.size(0))
+            target = ((1 - mask_time_indices.long()) * -100).transpose(0, 1).flatten()
+
+            contrastive_loss = torch.nn.functional.cross_entropy(
+                logits.float(), target, reduction="sum"
+            )
+            # 7. compute diversity loss: \mathbf{L}_d
+            num_codevectors = (
+                self.config.num_codevectors_per_group
+                * self.config.num_codevector_groups
+            )
+            diversity_loss = (
+                (num_codevectors - codevector_perplexity) / num_codevectors
+            ) * mask_time_indices.sum()
+
+            # 8. \mathbf{L} = \mathbf{L}_m + \alpha * \mathbf{L}_d
+            loss = contrastive_loss + self.config.diversity_loss_weight * diversity_loss
+        if not return_dict:
+            if loss is not None:
+                return (
+                    loss,
+                    transformer_features,
+                    quantized_features,
+                    codevector_perplexity,
+                ) + outputs[2:]
+            return (
+                transformer_features,
+                quantized_features,
+                codevector_perplexity,
+            ) + outputs[2:]
+
+        return Wav2Vec2ForPreTrainingOutput(
+            loss=loss,
+            projected_states=transformer_features,
+            projected_quantized_states=quantized_features,
+            codevector_perplexity=codevector_perplexity,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+            contrastive_loss=contrastive_loss,
+            diversity_loss=diversity_loss,
+        )
+
+
+class AdvHuggingFaceWav2Vec2Pretrain(HuggingFaceWav2Vec2Pretrain):
+    """This lobe enables the integration of HuggingFace
+     wav2vec2.0 models to be pretrained.
+     It also enables contrastive attacks and parameter loading from HuggingFace.
+
+    Arguments
+    ---------
+    source : str
+        HuggingFace hub name: e.g "facebook/wav2vec2-large-lv60"
+    save_path : str
+        Path (dir) of the downloaded model.
+    mask_prob : float (default: 0.65)
+        Probability of masking a given frame. Default is taken from the paper.
+    mask_length : float (default: 10)
+        Length (i.e. number of consecutive masked frames). Default is taken from
+        the paper.
+    """
+
+    def __init__(
+        self,
+        source,
+        save_path,
+        mask_prob=0.65,
+        mask_length=10,
+        normalize_wav=True,
+    ):
+        super(AdvHuggingFaceWav2Vec2Pretrain, self).__init__(
+            source,
+            save_path,
+            mask_prob=mask_prob,
+            mask_length=mask_length,
+            normalize_wav=normalize_wav,
+        )
+        self.model = AdvWav2Vec2ForPreTraining.from_pretrained(source)
+
+    def forward(self, wav, quantized_representation=None):
+        """Takes an input waveform and return its corresponding wav2vec encoding.
+
+        Arguments
+        ---------
+        wav : torch.Tensor (signal)
+            A batch of audio signals to transform to features.
+        quantized_representation : Optional[torch.Tensor,torch.Tensor]
+            A precomputed quantized representation of the audio signal.
+        """
+        batch_size, raw_sequence_length = wav.shape
+        if self.normalize_wav:
+            wav = F.layer_norm(wav, wav.shape)
+        sequence_length = self.model._get_feat_extract_output_lengths(
+            raw_sequence_length
+        )
+
+        # 1. Compute the indices that will be masked
+        mask_time_indices = _compute_mask_indices(
+            (batch_size, sequence_length),
+            mask_prob=self.mask_prob,
+            mask_length=self.mask_length,
+        )
+        torch_mask_time_indices = torch.tensor(
+            mask_time_indices,
+            device=wav.device,
+            dtype=torch.long,
+        )
+
+        # 2. Sample the negative samples from the entire sequence.
+        # Fairseq does it only on the masked indices, but this only work if you
+        # have long sentences. For more versatily, we sample on the entire sequence.
+        # value.
+        full_sentence_indices = np.ones((batch_size, sequence_length))
+        # print(np.sum(mask_time_indices, axis=1))
+        negative_sample_indices = torch.tensor(
+            transformers.models.wav2vec2.modeling_wav2vec2._sample_negative_indices(
+                (batch_size, sequence_length),
+                num_negatives=self.config.num_negatives,
+                mask_time_indices=full_sentence_indices,
+            ),
+            device=wav.device,
+            dtype=torch.long,
+        )
+        return (
+            self.model(
+                wav,
+                mask_time_indices=torch_mask_time_indices,
+                sampled_negative_indices=negative_sample_indices,
+                quantized_representation=quantized_representation,
+            ),
+            torch_mask_time_indices,
+        )
+
+class AdvWav2Vec2Model(Wav2Vec2Model):
+    """
+    This class modifies the transformers Wav2Vec2 module
+     in order to replace the Feature Extractor with AdvWav2Vec2FeatureEncoder
+    """
+
+    def __init__(self, config: Wav2Vec2Config):
+        super().__init__(config)
+        self.feature_extractor = AdvWav2Vec2FeatureEncoder(config)
+
+
+HF_models = {"wav2vec2": AdvWav2Vec2Model, "hubert": HubertModel}
+HF_config = {"wav2vec2": Wav2Vec2Config, "hubert": HubertConfig}
+
+
+class AdvHuggingFaceWav2Vec2(HuggingFaceWav2Vec2):
+    """This class inherits the SpeechBrain Wav2Vec2 lobe and
+    replaces the model with an AdvWav2Vec2 model,
+    which supports backpropagating through the inputs
+
+    Arguments
+    ---------
+    source : str
+        HuggingFace hub name: e.g "facebook/wav2vec2-large-lv60"
+    save_path : str
+        Path (dir) of the downloaded model.
+    output_norm : bool (default: True)
+        If True, a layer_norm (affine) will be applied to the output obtained
+        from the wav2vec model.
+    freeze : bool (default: True)
+        If True, the model is frozen. If False, the model will be trained
+        alongside with the rest of the pipeline.
+    freeze_feature_extractor :  bool (default: False)
+        When freeze = False and freeze_feature_extractor True,
+        the featue_extractor module of the model is Frozen. If False
+        all the wav2vec model will be trained including featue_extractor module.
+    apply_spec_augment : bool (default: False)
+        If True, the model will apply spec augment on the output of feature extractor
+        (inside huggingface Wav2VecModel() class).
+        If False, the model will not apply spec augment.
+        We set this to false to prevent from doing it twice.
+    """
+
+    def __init__(
+        self,
+        source,
+        save_path,
+        output_norm=True,
+        freeze=True,
+        freeze_feature_extractor=False,
+        apply_spec_augment=False,
+        load_pretrained_weights=True,
+    ):
+        nn.Module.__init__(self)
+
+        # Download the extractor from HuggingFace.
+        # The extractor is only used to retrieve the normalisation information
+        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
+            source, cache_dir=save_path
+        )
+
+        # Select specific self-supervised loader (eg. Wav2Vec2, Hubert)
+        if "hubert" in source:
+            config = HF_config.get("hubert")
+            model = HF_models.get("hubert")
+        else:
+            config = HF_config.get("wav2vec2")
+            model = HF_models.get("wav2vec2")
+
+        # Download and load the model
+        self._from_pretrained(source, config=config, model=model, save_path=save_path, load_weights=load_pretrained_weights)
+
+        # set apply_spec_augment
+        self.model.config.apply_spec_augment = apply_spec_augment
+
+        # We check if inputs need to be normalized w.r.t pretrained wav2vec2
+        self.normalize_wav = self.feature_extractor.do_normalize
+
+        self.freeze = freeze
+        self.freeze_feature_extractor = freeze_feature_extractor
+        self.output_norm = output_norm
+        if self.freeze:
+            logger.warning(
+                "speechbrain.lobes.models.huggingface_wav2vec - wav2vec 2.0 is frozen."
+            )
+            self.model.eval()
+            for param in self.model.parameters():
+                param.requires_grad = False
+        else:
+            self.model.train()
+            if self.freeze_feature_extractor:
+                self.model.feature_extractor._freeze_parameters()
+
--- robust_speech/models/wav2vec2_fine_tune.py
+++ robust_speech/models/wav2vec2_fine_tune.py
@@ -6,7 +6,6 @@ Inspired from SpeechBrain Wav2Vec2
 (https://github.com/speechbrain/speechbrain/blob/develop/recipes/LibriSpeech/ASR/CTC/train_with_wav2vec.py)
 """
 
-import gc
 import logging
 import os
 import sys
@@ -25,103 +24,8 @@ from transformers import (
 
 import robust_speech as rs
 from robust_speech.adversarial.brain import AdvASRBrain
-from robust_speech.models.wav2vec2_pretrain import AdvWav2Vec2FeatureEncoder
 
 logger = logging.getLogger(__name__)
-
-
-class AdvWav2Vec2Model(Wav2Vec2Model):
-    """
-    This class modifies the transformers Wav2Vec2 module
-     in order to replace the Feature Extractor with AdvWav2Vec2FeatureEncoder
-    """
-
-    def __init__(self, config: Wav2Vec2Config):
-        super().__init__(config)
-        self.feature_extractor = AdvWav2Vec2FeatureEncoder(config)
-
-
-HF_models = {"wav2vec2": AdvWav2Vec2Model, "hubert": HubertModel}
-HF_config = {"wav2vec2": Wav2Vec2Config, "hubert": HubertConfig}
-
-
-class AdvHuggingFaceWav2Vec2(HuggingFaceWav2Vec2):
-    """This class inherits the SpeechBrain Wav2Vec2 lobe and
-    replaces the model with an AdvWav2Vec2 model,
-    which supports backpropagating through the inputs
-
-    Arguments
-    ---------
-    source : str
-        HuggingFace hub name: e.g "facebook/wav2vec2-large-lv60"
-    save_path : str
-        Path (dir) of the downloaded model.
-    output_norm : bool (default: True)
-        If True, a layer_norm (affine) will be applied to the output obtained
-        from the wav2vec model.
-    freeze : bool (default: True)
-        If True, the model is frozen. If False, the model will be trained
-        alongside with the rest of the pipeline.
-    freeze_feature_extractor :  bool (default: False)
-        When freeze = False and freeze_feature_extractor True,
-        the featue_extractor module of the model is Frozen. If False
-        all the wav2vec model will be trained including featue_extractor module.
-    apply_spec_augment : bool (default: False)
-        If True, the model will apply spec augment on the output of feature extractor
-        (inside huggingface Wav2VecModel() class).
-        If False, the model will not apply spec augment.
-        We set this to false to prevent from doing it twice.
-    """
-
-    def __init__(
-        self,
-        source,
-        save_path,
-        output_norm=True,
-        freeze=True,
-        freeze_feature_extractor=False,
-        apply_spec_augment=False,
-        load_pretrained_weights=True,
-    ):
-        nn.Module.__init__(self)
-
-        # Download the extractor from HuggingFace.
-        # The extractor is only used to retrieve the normalisation information
-        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
-            source, cache_dir=save_path
-        )
-
-        # Select specific self-supervised loader (eg. Wav2Vec2, Hubert)
-        if "hubert" in source:
-            config = HF_config.get("hubert")
-            model = HF_models.get("hubert")
-        else:
-            config = HF_config.get("wav2vec2")
-            model = HF_models.get("wav2vec2")
-
-        # Download and load the model
-        self._from_pretrained(source, config=config, model=model, save_path=save_path, load_weights=load_pretrained_weights)
-
-        # set apply_spec_augment
-        self.model.config.apply_spec_augment = apply_spec_augment
-
-        # We check if inputs need to be normalized w.r.t pretrained wav2vec2
-        self.normalize_wav = self.feature_extractor.do_normalize
-
-        self.freeze = freeze
-        self.freeze_feature_extractor = freeze_feature_extractor
-        self.output_norm = output_norm
-        if self.freeze:
-            logger.warning(
-                "speechbrain.lobes.models.huggingface_wav2vec - wav2vec 2.0 is frozen."
-            )
-            self.model.eval()
-            for param in self.model.parameters():
-                param.requires_grad = False
-        else:
-            self.model.train()
-            if self.freeze_feature_extractor:
-                self.model.feature_extractor._freeze_parameters()
 
 
 # Define training procedure
@@ -214,34 +118,6 @@ class W2VASR(AdvASRBrain):
 
         return loss
 
-    def fit_batch(self, batch):
-        """Train the parameters given a single batch in input"""
-        predictions = self.compute_forward(batch, sb.Stage.TRAIN)
-        loss = self.compute_objectives(predictions, batch, sb.Stage.TRAIN)
-        loss.backward()
-        if self.check_gradients(loss):
-            self.wav2vec_optimizer.step()
-            self.model_optimizer.step()
-
-        self.wav2vec_optimizer.zero_grad()
-        self.model_optimizer.zero_grad()
-
-        return loss.detach().cpu()
-
-    def fit_batch_adversarial(self, batch):
-        """Train the parameters given a single batch in input"""
-        predictions, _ = self.compute_forward_adversarial(batch, sb.Stage.TRAIN)
-        loss = self.compute_objectives(predictions, batch, sb.Stage.TRAIN)
-        loss.backward()
-        if self.check_gradients(loss):
-            self.wav2vec_optimizer.step()
-            self.model_optimizer.step()
-
-        self.wav2vec_optimizer.zero_grad()
-        self.model_optimizer.zero_grad()
-
-        return loss.detach().cpu()
-
     def on_stage_end(
         self, stage, stage_loss, epoch, stage_adv_loss=None, stage_adv_loss_target=None
     ):
@@ -270,21 +146,12 @@ class W2VASR(AdvASRBrain):
 
         # Perform end-of-iteration things, like annealing, logging, etc.
         if stage == sb.Stage.VALID:
-            old_lr_model, new_lr_model = self.hparams.lr_annealing_model(
-                stage_stats["loss"]
-            )
-            old_lr_wav2vec, new_lr_wav2vec = self.hparams.lr_annealing_wav2vec(
-                stage_stats["loss"]
-            )
-            sb.nnet.schedulers.update_learning_rate(self.model_optimizer, new_lr_model)
-            sb.nnet.schedulers.update_learning_rate(
-                self.wav2vec_optimizer, new_lr_wav2vec
-            )
+            current_lr = self.hparams.noam_annealing.current_lr
+            steps = self.hparams.noam_annealing.n_steps
             self.hparams.train_logger.log_stats(
                 stats_meta={
                     "epoch": epoch,
-                    "lr_model": old_lr_model,
-                    "lr_wav2vec": old_lr_wav2vec,
+                    "lr_model": current_lr,
                 },
                 train_stats=self.train_stats,
                 valid_stats=stage_stats,
@@ -303,13 +170,31 @@ class W2VASR(AdvASRBrain):
 
     def init_optimizers(self):
         "Initializes the wav2vec2 optimizer and model optimizer"
-        self.wav2vec_optimizer = self.hparams.wav2vec_opt_class(
-            self.modules.wav2vec2.parameters()
-        )
-        self.model_optimizer = self.hparams.model_opt_class(
+        self.optimizer = self.hparams.opt_class(
             self.hparams.model.parameters()
         )
 
         if self.checkpointer is not None:
-            self.checkpointer.add_recoverable("wav2vec_opt", self.wav2vec_optimizer)
-            self.checkpointer.add_recoverable("modelopt", self.model_optimizer)
+            self.checkpointer.add_recoverable("optimizer", self.optimizer)
+
+    def fit_batch(self, batch):
+        """Train the parameters given a single batch in input"""
+        # check if we need to switch optimizer
+        # if so change the optimizer from Adam to SGD
+
+        predictions = self.compute_forward(batch, sb.Stage.TRAIN)
+        loss = self.compute_objectives(predictions, batch, sb.Stage.TRAIN)
+
+        # normalize the loss by gradient_accumulation step
+        (loss / self.hparams.gradient_accumulation).backward()
+
+        if self.step % self.hparams.gradient_accumulation == 0:
+            # gradient clipping & early stop if loss is not fini
+            self.check_gradients(loss)
+
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            # anneal lr every update
+            self.hparams.noam_annealing(self.optimizer)
+        return loss.detach()

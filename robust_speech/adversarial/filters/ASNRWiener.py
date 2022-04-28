48319e6790b662458811c43f6dfc0850f209b929
Urvashi Kumar <upkumar@andrew.cmu.edu>
2022-04-27 23:54:15 -0400

    fix grad

--- robust_speech/adversarial/filters/ASNRWiener.py
+++ robust_speech/adversarial/filters/ASNRWiener.py
@@ -22,24 +22,22 @@ class ASNRWiener:
         if 'high_freq' in filter_config:
             self.high_freq=filter_config['high_freq']
 
-    def __call__(self, x: np.ndarray):
+    def __call__(self, x):
         print("ASNR filter launched ", x.shape)
-        device=x.get_device()
-        x=x.cpu().detach().numpy()
-        x_filtered=np.copy(x)
-        for i in range(len(x)):
+        x_np=x.cpu().detach().numpy()
+        for i in range(len(x_np)):
             if self.high_freq:
-                noise = np.random.normal(0, scale=self.gaussian_sigma, size=(x[i].shape[0]+1,))
+                noise = np.random.normal(0, scale=self.gaussian_sigma, size=(x_np[i].shape[0]+1,))
                 noise = 0.5 * (noise[1:]-noise[:-1])
             else:
-                noise = np.random.normal(0, scale=self.gaussian_sigma, size=x[i].shape).astype(ART_NUMPY_DTYPE) if self.gaussian_sigma else None
-            filtered_output,_=asnr(x[i],self.sr, self.window, self.hop, self.nfft,noise=(noise if self.gaussian_sigma>0 else None),
+                noise = np.random.normal(0, scale=self.gaussian_sigma, size=x_np[i].shape).astype(ART_NUMPY_DTYPE) if self.gaussian_sigma else None
+            filtered_output,_=asnr(x_np[i],self.sr, self.window, self.hop, self.nfft,noise=(noise if self.gaussian_sigma>0 else None),
             snrsmooth=0.98, noisesmooth=0.98, llkthres=.15, zphase=True, rule="wiener")
-            if len(filtered_output)<len(x[i]):
-                filtered_output = np.pad(filtered_output,mode="mean",pad_width=((0,len(x[i])-len(filtered_output))))
-            elif len(filtered_output)>len(x[i]):
-                filtered_output=filtered_output[:len(x[i])]
-            x_filtered[i]=filtered_output
-        print("ASNR filter finished ", x_filtered.shape)
-        x_filtered = torch.from_numpy(x_filtered).to(device)
-        return x_filtered
+            if len(filtered_output)<len(x_np[i]):
+                filtered_output = np.pad(filtered_output,mode="mean",pad_width=((0,len(x_np[i])-len(filtered_output))))
+            elif len(filtered_output)>len(x_np[i]):
+                filtered_output=filtered_output[:len(x_np[i])]
+            x[i]=torch.from_numpy(filtered_output)
+        print("ASNR filter finished ", x.shape)
+        # x_filtered = torch.from_numpy(x_filtered).to(device)
+        return x

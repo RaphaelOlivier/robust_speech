e5b580de8991b420a20b9590a5818980666fdba1
JamesLee <first287@naver.com>
2022-05-03 01:22:44 +0900

    Time universal

--- recipes/ueval_up.sh
+++ recipes/ueval_up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python universal_evaluate.py attack_configs/universal/ctc_5000bpe_universal.yaml --data_csv_name=test-clean-short --root=/home/deokhk/coursework/robust_speech/root --nb_iter=10 --eps=0.2 --lr 0.001 --seed=1026
+python universal_evaluate.py attack_configs/universal/ctc_5000bpe_universal.yaml --data_csv_name=test-clean-short --root=/home/cvlab/robust/robust_speech/root --nb_iter=10 --eps=0.2 --lr 0.001 --seed=1026

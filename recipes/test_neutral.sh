2a76efd18262a69532181ac875588dfbd191d954
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2022-05-20 22:17:26 -0400

    remove superfluous changes from fork

--- recipes/test_neutral.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python /home/deokhk/coursework/robust_speech/recipes/evaluate.py attack_configs/none/ctc_5000bpe.yaml --data_csv_name=test-clean-adv-100-dev --root=/home/deokhk/coursework/robust_speech/root 
